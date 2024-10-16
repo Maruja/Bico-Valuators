9f7180fe47a75cdf276dce78300c486040ea23a5
Maruja <maria.eugenia.lopez.ramirez11@gmail.com>
2024-10-16 16:51:47 -0700

    Feature: Se agrego MongoDB Atlas como DB,  tambien se hizo el delete simple.

--- src/main/java/com/example/bico/appraiser/controller/AppraiserController.java
+++ src/main/java/com/example/bico/appraiser/controller/AppraiserController.java
@@ -2,10 +2,12 @@ package com.example.bico.appraiser.controller;
 
 import com.example.bico.appraiser.model.Appraiser;
 import com.example.bico.appraiser.service.ServiceAppraiser;
-import com.example.bico.appraiser.service.implementation.ServiceAppraiserMemory;
-import com.example.bico.appraiser.util.BicoUtilities;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +22,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/appraisers")
 class AppraiserController {
+    private static final Logger logger = LoggerFactory.getLogger(AppraiserController.class);
     @Autowired
     private ServiceAppraiser serviceAppraiser;
     @GetMapping("/{requestedId}")
@@ -29,7 +32,7 @@ class AppraiserController {
     }
 
     @PostMapping
-    private ResponseEntity<Void> createAppraiser(@RequestBody Appraiser newAppraiser) {
+    private ResponseEntity<Void> createAppraiser(@RequestBody @Valid Appraiser newAppraiser) {
         String uri = serviceAppraiser.addAppraiser(newAppraiser);
         return ResponseEntity.created(URI.create(uri)).build();
     }
@@ -37,5 +40,11 @@ class AppraiserController {
     @GetMapping("")
     private ResponseEntity<List<Appraiser>> findAll() {
         return ResponseEntity.ok(serviceAppraiser.getAll());
+    }
+
+    @DeleteMapping("/{deleteId}")
+    private ResponseEntity<Void> deleteAppraiser(@PathVariable String deleteId){
+        serviceAppraiser.deleteAppraiser(deleteId);
+        return ResponseEntity.noContent().build();
     }
 }
