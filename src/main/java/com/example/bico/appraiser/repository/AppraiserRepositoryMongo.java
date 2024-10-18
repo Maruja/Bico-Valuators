5f89ffc36126a428519cf35d8fe360639542e690
Maria Eugenia de los Angeles Lopez Ramirez <maria.eugenia.lopez.ramirez11@gmail.com>
2024-10-18 12:50:20 -0700

    Feature: Added Soft delete to appraisers. (#2)

--- src/main/java/com/example/bico/appraiser/repository/AppraiserRepositoryMongo.java
+++ src/main/java/com/example/bico/appraiser/repository/AppraiserRepositoryMongo.java
@@ -4,6 +4,10 @@ import com.example.bico.appraiser.model.mongo.AppraiserDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AppraiserRepositoryMongo extends MongoRepository<AppraiserDocument, String> {
+    // Query in mongodb to find all entities that are deleted or not depending on boolean "deleted"
+    List<AppraiserDocument> findByDeleted(boolean deleted);
 }
